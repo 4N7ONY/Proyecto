6d0bff729dd7b4b10dd9bb1aa5e10c6212abbc17
4N7ONY <amalcachilon@gmail.com>
2025-10-20 16:09:33 -0500

    v1

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -25,7 +25,7 @@ namespace Version_2._0_Proyecto
                 Console.Clear();
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("═══════════════════════════════════════════════════════════");
-                Console.WriteLine("      🔥 SISTEMA CONTRA INCENDIOS INTELIGENTE - SCI 🔥");
+                Console.WriteLine("       SISTEMA CONTRA INCENDIOS INTELIGENTE - SCI ");
                 Console.WriteLine("═══════════════════════════════════════════════════════════");
                 Console.ResetColor();
                 Console.WriteLine("[1] Mostrar zonas de sensores");
